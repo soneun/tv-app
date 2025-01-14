b1ac661d931fad33871727fed75f50457e70f36c
EJ <harrygina09@naver.com>
2025-01-14 09:57:24 +0900

    프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">THE MOVIE</nav>
+
+      <main>Main Content</main>
+    </div>
+  );
+}
+
+export default App;
